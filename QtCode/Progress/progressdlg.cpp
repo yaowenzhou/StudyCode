a446c4c740f33a344319df9b2bd0583fe7805112
姚文洲 <60352284+yaowenzhou@users.noreply.github.com>
2020-05-02 16:32:23 +0800

    Revert "Revert "First commit""
    This reverts commit 643c7a89185ab3a1e8b4bfc5e922416228b12aee.

---
+++ QtCode/Progress/progressdlg.cpp
@@ -0,0 +1,73 @@
+﻿//此案例充分证明了槽函数的阻塞性
+#include "progressdlg.h"
+#include <QProgressDialog>
+#include <QFont>
+#include <QPushButton>
+
+ProgressDlg::ProgressDlg(QWidget *parent)
+    : QDialog(parent)
+{
+    QFont font("ZYSong18030", 12);
+    setFont(font);
+    setWindowTitle("Progress");
+
+    fileNum = new QLabel("文件数目：");
+    fileNumLineEdit = new QLineEdit("100000");
+    progressType = new QLabel("显示类型：");
+    comboBox = new QComboBox;
+    comboBox->addItem("progressBar");
+    comboBox->addItem("progressDialog");
+    progressBar = new QProgressBar;
+    progressBar->setValue(0);
+    startBtn = new QPushButton("开始");
+
+    mainLayout = new QGridLayout(this);
+    mainLayout->addWidget(fileNum, 0, 0);
+    mainLayout->addWidget(fileNumLineEdit, 0, 1);
+    mainLayout->addWidget(progressType, 1, 0);
+    mainLayout->addWidget(comboBox, 1, 1);
+    mainLayout->addWidget(progressBar, 2, 0, 1, 2);
+    mainLayout->addWidget(startBtn, 3, 1);
+    mainLayout->setMargin(15);
+    mainLayout->setSpacing(10);
+    connect(startBtn, &QPushButton::clicked, this, &ProgressDlg::startProgress);
+
+}
+
+ProgressDlg::~ProgressDlg()
+{
+}
+
+void ProgressDlg::startProgress()
+{
+    bool ok;
+    int num = fileNumLineEdit->text().toInt(&ok);
+    if(comboBox->currentIndex() == 0)
+    {
+        progressBar->setRange(0, num);
+        for (int i = 1; i < num + 1; ++i)
+        {
+            progressBar->setValue(i);
+        }
+    }
+    else if (comboBox->currentIndex() == 1)
+    {
+        //创建一个进度对话框
+        QProgressDialog *progressDialog = new QProgressDialog(this);
+        QFont font("ZYSong18030", 12);
+        progressDialog->setFont(font);
+        progressDialog->setWindowModality(Qt::WindowModal);
+        progressDialog->setMinimumDuration(5);
+        progressDialog->setWindowTitle("Please Wait");
+        progressDialog->setLabelText("Copying...");
+        progressDialog->setCancelButtonText("Cancel");
+        progressDialog->setRange(0, num);
+        for (int i = 1; i < num + 1; ++i)
+        {
+            progressDialog->setValue(i);
+            if(progressDialog->wasCanceled())
+                return;
+        }
+    }
+}
+
---
+++ QtCode/Progress/progressdlg.h
@@ -0,0 +1,31 @@
+﻿#ifndef PROGRESSDLG_H
+#define PROGRESSDLG_H
+
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QProgressBar>
+#include <QComboBox>
+#include <QPushButton>
+#include <QGridLayout>
+
+class ProgressDlg : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ProgressDlg(QWidget *parent = nullptr);
+    ~ProgressDlg();
+
+private slots:
+    void startProgress();
+private:
+    QLabel *fileNum;
+    QLineEdit *fileNumLineEdit;
+    QLabel *progressType;
+    QComboBox *comboBox;
+    QProgressBar *progressBar;
+    QPushButton *startBtn;
+    QGridLayout *mainLayout;
+};
+#endif // PROGRESSDLG_H
